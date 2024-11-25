5116546b79a2aac53563ae401c1d21b52469f5ad
T-Al-D <t.private@gmx.net>
2024-11-25 16:03:44 +0100

    changed main.py, seperated functions, more comments

--- main.py
+++ main.py
@@ -9,49 +9,77 @@ from Classes.SoccerRobot import SoccerRobot
 from Constants import Const
 
 
+def ballMovementsThroughCollision(
+    keys: pygame.key,
+    playerRectangles: pygame.Rect,
+    ballRect: pygame.Rect,
+    field: SoccerField,
+):
+    # ball movement through collision
+    collisionDetect = ballRect.collidelist(playerRectangles)
+    if collisionDetect >= 0:
+        print(f"ball: X {field.ballPositionX} and Y {field.ballPositionY}")
+        print(collisionDetect)
+        ballSteps = ballRect.width / 2
+        if keys[pygame.K_LEFT]:
+            field.moveBall(Const.WEST_DIRECTION, ballSteps)
+        elif keys[pygame.K_RIGHT]:
+            field.moveBall(Const.EAST_DIRECTION, ballSteps)
+        elif keys[pygame.K_UP]:
+            field.moveBall(Const.NORTH_DIRECTION, ballSteps)
+        elif keys[pygame.K_DOWN]:
+            field.moveBall(Const.SOUTH_DIRECTION, ballSteps)
+
+
+def playerMovement(keys: pygame.key, player: SoccerRobot):
+    # Move the player based on key pressed
+    if keys[pygame.K_LEFT]:
+        player.move(Const.WEST_DIRECTION, playerSteps)
+    elif keys[pygame.K_RIGHT]:
+        player.move(Const.EAST_DIRECTION, playerSteps)
+    elif keys[pygame.K_UP]:
+        player.move(Const.NORTH_DIRECTION, playerSteps)
+    elif keys[pygame.K_DOWN]:
+        player.move(Const.SOUTH_DIRECTION, playerSteps)
+
+
 # draw the player
 def drawPlayer(displayScreen: pygame.Surface, playerRobot: SoccerRobot, img):
     # create a playerRectangle
-    playerRect = pygame.Rect(playerRobot.positionX, playerRobot.positionY, playerRobot.playerSize, playerSize)
+    playerRect = pygame.Rect(
+        playerRobot.positionX, playerRobot.positionY, playerRobot.playerSize, playerSize
+    )
     # assign the playerRectangle to a specific image and position, then draw the rect
-    displayScreen.blit(img, playerRect.topleft)
+    displayScreen.blit(img, playerRect)
+
     # Optional: Draw a border around the rectangle (for visualization)
-    pygame.draw.rect(screen, (0, 0, 0), playerRect, 1)
+    # color = (0, 0, 0)
+    # pygame.draw.rect(screen, color, playerRect, 1)
+
+    # return playerRectangle for collision detection
+    return playerRect
 
 
 # complete game logic
-def gameIsRunning(displayScreen: pygame.Surface, field: SoccerField, player1: SoccerRobot, player2: SoccerRobot,
-                  playerImg):
+def gameIsRunning(
+    fps: int,
+    displayScreen: pygame.Surface,
+    field: SoccerField,
+    player1: SoccerRobot,
+    player2: SoccerRobot,
+    playerImg,
+):
     # import global variable
-    global frames, clock
-    frames += 1
+    global clock
 
     # limit frame rate
-    clock.tick(75)
+    clock.tick(fps)
 
     # get the key that are currently pressed
     keys = pygame.key.get_pressed()
 
-    # Move the player based on key presses
-    if keys[pygame.K_LEFT]:
-        player1.move(Const.WEST_DIRECTION, playerSteps)
-    if keys[pygame.K_RIGHT]:
-        player1.move(Const.EAST_DIRECTION, playerSteps)
-    if keys[pygame.K_UP]:
-        player1.move(Const.NORTH_DIRECTION, playerSteps)
-    if keys[pygame.K_DOWN]:
-        player1.move(Const.SOUTH_DIRECTION, playerSteps)
-
-    # ball movement through collision
-    if (player1.positionX - (player1.playerSize / 2) < field.ballPositionX < player1.positionX + (
-            player1.playerSize / 2)) and (
-            player1.positionY < field.ballPositionY < player1.positionY + player1.playerSize):
-        print(f"player1: X {player1.positionX} and Y {player1.positionY}")
-        print(f"ball: X {field.ballPositionX} and Y {field.ballPositionY}")
-        if keys[pygame.K_LEFT]:
-            field.moveBall(Const.WEST_DIRECTION, playerSteps * 3)
-        if keys[pygame.K_RIGHT]:
-            field.moveBall(Const.EAST_DIRECTION, playerSteps * 3)
+    # movement for player1 (soccerRobots)
+    playerMovement(keys, player1)
 
     # random choice for movement for player2
     randomMove = random.choice(Const.allBasicDirections)
@@ -61,13 +89,24 @@ def gameIsRunning(displayScreen: pygame.Surface, field: SoccerField, player1: So
     displayScreen.blit(backgroundImg, (0, 0))
 
     # draw the ball and assign rectangle to it
-    ballRect = pygame.Rect(field.ballPositionX, field.ballPositionY, field.ballSize, field.ballSize)
-    displayScreen.blit(ballImg, ballRect.topleft)
-    pygame.draw.rect(screen, (0, 0, 0), ballRect, 1)
+    ballRect = pygame.Rect(
+        field.ballPositionX, field.ballPositionY, field.ballSize, field.ballSize
+    )
+    displayScreen.blit(ballImg, ballRect)
+    # Optional
+    # color = (0, 0, 0)
+    # pygame.draw.rect(screen, color, ballRect, 1)
+
+    # playerRectangles for collision detection
+    playerRectangles = []
 
     # draw all the player on the field
     for player in soccerField.players:
-        drawPlayer(displayScreen, player, playerImg)
+        playerRect = drawPlayer(displayScreen, player, playerImg)
+        playerRectangles.append(playerRect)
+
+    # collision
+    ballMovementsThroughCollision(keys, playerRectangles, ballRect, field)
 
     # Update the display
     pygame.display.flip()
@@ -95,7 +134,9 @@ if __name__ == "__main__":
 
     # load images
     backgroundImg = pygame.image.load("resources/SoccerFieldBackground2.jpg")
-    backgroundImg = pygame.transform.scale(backgroundImg, (screenWidth, screenHeight))  # Scale to fit the window
+    backgroundImg = pygame.transform.scale(
+        backgroundImg, (screenWidth, screenHeight)
+    )  # Scale to fit the window
     soccerRobotImg = pygame.image.load("resources/SoccerRobotPlayer.png")
     soccerRobotImg = pygame.transform.scale(soccerRobotImg, (playerSize, playerSize))
     ballImg = pygame.image.load("resources/Ball2.png")
@@ -103,15 +144,26 @@ if __name__ == "__main__":
 
     # objects
     soccerField = SoccerField(0, screenWidth, screenHeight, 1, ballSize)
-    soccerRobot1 = SoccerRobot(1, 1, playerSize, playerSteps, int(screenWidth * 0.82), int(screenHeight / 2.15),
-                               soccerField)
-    soccerRobot2 = SoccerRobot(2, 2, playerSize, playerSteps, int(screenWidth * 0.1), int(screenHeight / 2.15),
-                               soccerField)
+    soccerRobot1 = SoccerRobot(
+        1,
+        1,
+        playerSize,
+        playerSteps,
+        int(screenWidth * 0.82),
+        int(screenHeight / 2.15),
+        soccerField,
+    )
+    soccerRobot2 = SoccerRobot(
+        2,
+        2,
+        playerSize,
+        playerSteps,
+        int(screenWidth * 0.1),
+        int(screenHeight / 2.15),
+        soccerField,
+    )
     soccerField.players.append(soccerRobot1)
     soccerField.players.append(soccerRobot2)
-
-    # counts the frames during the simulation (if needed)
-    frames = 0
 
     # get clock for pygame
     clock = pygame.time.Clock()
@@ -127,7 +179,9 @@ if __name__ == "__main__":
                 running = False
 
         # all game logic is in here
-        gameIsRunning(screen, soccerField, soccerRobot1, soccerRobot2, soccerRobotImg)
+        gameIsRunning(
+            75, screen, soccerField, soccerRobot1, soccerRobot2, soccerRobotImg
+        )
 
     # quit Pygame
     pygame.quit()
