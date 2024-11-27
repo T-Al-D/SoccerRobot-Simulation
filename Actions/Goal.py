f69b73c64acff485515add27d7927364c5bc2299
BluMay <blumay1212@gmail.com>
2024-11-27 14:27:02 +0100

    Goal

---
+++ Actions/Goal.py
@@ -0,0 +1,18 @@
+import pygame
+
+from Classes.SoccerField import SoccerField
+from Constants import Const
+
+
+goal_counter = 0
+
+def checkBallPosition(ballRect: pygame.Rect, goal_counter: int):
+
+    ballRect: pygame.Rect
+    field: SoccerField
+
+    if field.ballPositionX > 750 or field.ballPositionX < 20:
+        goal_counter += 1
+        print(f"Tor-Ereignis! Neuer Stand: {goal_counter}")
+    return goal_counter
+
